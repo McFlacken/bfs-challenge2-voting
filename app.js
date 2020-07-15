8f7f256164df3ee27b59ac94065a3ff0c289cdaf
McFlacken <angel.i.martinez@gmail.com>
2020-07-15 01:12:32 +0000

    switch to vanilla JS for this version of the challenge

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-var express = require("express");
-var app = express();
-var request = require("request");
+// GET REQUEST
+function getCat() {
+  axios
+    .get('https://api.thecatapi.com/v1/images/search')
+    .then(res => showCat(res))
+    .catch(err => console.log(error));
+}
 
-app.set("view engine", "ejs");
+getCat();
 
-app.get("/", (req,res)=>{
-	   res.redirect("/home");
-});
+//this will show the cat in the inner HTML
+function showCat(res){
+  document.getElementById('gato').innerHTML = `
+  <img class="card-img-top" src='${res.data[0]["url"]}'>
+  `;
+  console.log(res.data[0]["id"]); //I know this is the id of the image I would like to vote on... I just don't know how to get it to the POST function... see below
+}
 
-app.get("/home", function(req, res){
+// POST REQUEST
+function votedUgly() {
+  axios
+    .post('https://api.thecatapi.com/v1/votes', {
+      "image_id": "${data[0]["id"]}" //I don't know how to call the data of the image called in the get function... how do I call it back?
+      "sub_id": "my-user-1234", //sub-id I guess can be set arbitrary, or not at all?? since there is no user system set up for this demo?
+      "value": 1 //this would make the vote go to the pile of 1's, the value in the other button when set to 0, would make the vote go to the pile of 0's I guess?
+    },
+    {
+      "headers": {'x-api-key': '6c8d8157-9c52-4111-8e6e-8d866cf41af5'} //I struggled with adding this, I was missing a pair of quotes
+    })
+    .then(res => console.log(res)) //this is pending... I haven't got this far, maybe it would be good to console log a success message, but haven't gotten to this yet.
+    .catch(err => console.error(err));
+}
 
-	request('https://api.thecatapi.com/v1/images/search', function(error, response, body){
-	if(!error && response.statusCode == 200){
-		var parsedData = JSON.parse(body);
-		res.render("home", {data: parsedData})
-		console.log(parsedData[0]["url"]);
-	}
-	});
-});
-
-// app.post("/blogs", function(req, res){
-// 	req.body.blog.body = req.sanitize(req.body.blog.body);
-// 	Blog.create(req.body.blog, function(err, newBlog){
-// 		if(err){
-// 			console.log("ooohhh nein...");
-// 			console.log(err);
-// 		} else {
-// 			res.redirect("/home");
-// 		}
-// 	})
-// })
+// POST REQUEST
+function votedUglier() {
+  console.log("voted for uglier");
+}
 
 
+function getVotes(){
+  console.log("getting the votes....")
+}
 
-app.listen(process.env.PORT || 3000, function(){
-	console.log('In a galaxy far far away... CAT app has now started...');
-});
+
+
+
+// Event listeners
+document.getElementById('show-a-cat').addEventListener('click', getCat);
+document.getElementById('ugly').addEventListener('click', votedUgly);
+document.getElementById('uglier').addEventListener('click', votedUglier);
+document.getElementById('check-votes').addEventListener('click', getVotes);
