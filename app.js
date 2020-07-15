2e5e9f20b6286ecb0d30a4774b2cc28430c54b77
McFlacken <angel.i.martinez@gmail.com>
2020-07-15 15:08:48 +0000

    fix the POST request in the voting

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+let voteData = {  //all this is dummy data, will be modified later on, as needed
+  "image_id": "asf2",
+  "sub_id": "my-user-1234",
+  "value": 1
+}
+
+
 // GET REQUEST
 function getCat() {
   axios
@@ -13,27 +20,35 @@ function showCat(res){
   document.getElementById('gato').innerHTML = `
   <img class="card-img-top" src='${res.data[0]["url"]}'>
   `;
-  console.log(res.data[0]["id"]); //I know this is the id of the image I would like to vote on... I just don't know how to get it to the POST function... see below
+  console.log(res.data[0]["id"]);
+  voteData.image_id = res.data[0]["id"];
+  console.log(voteData.image_id);
 }
 
 // POST REQUEST
 function votedUgly() {
   axios
-    .post('https://api.thecatapi.com/v1/votes', {
-      //"image_id": "${data[0]["id"]}" //I don't know how to call the data of the image called in the get function... how do I call it back?
-      "sub_id": "my-user-1234", //sub-id I guess can be set arbitrary, or not at all?? since there is no user system set up for this demo?
-      "value": 1 //this would make the vote go to the pile of 1's, the value in the other button when set to 0, would make the vote go to the pile of 0's I guess?
-    },
+    .post('https://api.thecatapi.com/v1/votes', voteData,
     {
-      "headers": {'x-api-key': '6c8d8157-9c52-4111-8e6e-8d866cf41af5'} //I struggled with adding this, I was missing a pair of quotes
+      "headers": {'x-api-key': '6c8d8157-9c52-4111-8e6e-8d866cf41af5'}
     })
-    .then(res => console.log(res)) //this is pending... I haven't got this far, maybe it would be good to console log a success message, but haven't gotten to this yet.
-    .catch(err => console.error(err));
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+    ;
+  getCat();
 }
 
 // POST REQUEST
 function votedUglier() {
-  console.log("voted for uglier");
+  voteData.value = 0;
+  axios
+    .post('https://api.thecatapi.com/v1/votes', voteData,
+    {
+      "headers": {'x-api-key': '6c8d8157-9c52-4111-8e6e-8d866cf41af5'}
+    })
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+  getCat();
 }
 
 
